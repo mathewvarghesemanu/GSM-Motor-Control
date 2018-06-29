dcc319241bfaee64e709e9a4effb623c33f1d399
Mathew Varghese <mathewvarghesemanu@gmail.com>
2018-06-29 22:21:01 +0530

    init copy

--- GSM_Motor_Control/GSM_Motor_Control.ino
+++ GSM_Motor_Control/GSM_Motor_Control.ino
@@ -1,9 +1,24 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+  The circuit:
+  RX is digital pin 10 (connect to TX of GSM Modem)
+  TX is digital pin 11 (connect to RX of GSM Modem)
+*/
+#include <SoftwareSerial.h>
 
+SoftwareSerial mySerial(10, 11); // RX, TX
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Calling through GSM Modem");
+  mySerial.begin(9600);
+  delay(2000);
+  mySerial.println("ATD81290255XX;"); // ATD81290255XX; -- watch out here for semicolon at the end!!
+  Serial.println("Called ATD81290255XX");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop()
+{
+  if (mySerial.available())
+    Serial.write(mySerial.read());
 }
